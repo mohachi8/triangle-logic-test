0c53088afc086b89708b4ae101d1d3cac3903d46
mohachi8 <maedoimitsuaki@icloud.com>
2024-10-02 14:42:16 +0900

    フォルダーを整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Header from "./components/Header";
-import { Typography } from "@mui/material";
-import LabeledTriangle from "./components/LabeledTriangle";
 
 const theme = createTheme({
   palette: {
@@ -23,21 +21,7 @@ function App() {
       {/* ヘッダー */}
       <Header />
       {/* コンテンツ */}
-      <div
-        style={{
-          display: "flex", // Flexboxを使用
-          flexDirection: "column", // 子要素を縦に並べる
-          alignItems: "center", // 子要素を中央揃えにするÏ
-          width: "100%", // 全幅を使用
-          padding: "20px", // 任意のパディング
-        }}
-      >
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          以下の命題から三角ロジックを組み立てなさい。
-        </Typography>
-        {/* <Triangle sizeVW={60} color="#808080" /> 三角形を表示 */}
-        <LabeledTriangle sizeVW={40} color="#808080" />
-      </div>
+
     </ThemeProvider>
   );
 }
---
+++ src/layouts/learning-screens/step1/index.tsx
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material";
+import LabeledTriangle from "../../../components/LabeledTriangle";
+
+function Step1() {
+  return (
+    <div
+      style={{
+        display: "flex", // Flexboxを使用
+        flexDirection: "column", // 子要素を縦に並べる
+        alignItems: "center", // 子要素を中央揃えにするÏ
+        width: "100%", // 全幅を使用
+        padding: "20px", // 任意のパディング
+      }}
+    >
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        以下の命題から三角ロジックを組み立てなさい。
+      </Typography>
+      {/* <Triangle sizeVW={60} color="#808080" /> 三角形を表示 */}
+      <LabeledTriangle sizeVW={40} color="#808080" />
+    </div>
+  );
+}
+
+export default Step1;
